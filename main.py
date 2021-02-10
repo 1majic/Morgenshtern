1ec5c13dfc92285d421421164a62dc9502303f04
User <nizamov.dinislam.04@mail.ru>
2021-02-10 17:26:14 +0500

    Working!

--- main.py
+++ main.py
@@ -3,27 +3,33 @@ import sys
 
 from PyQt5 import uic
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPainter, QColor, QPen
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor, QPen, QPixmap, QBrush
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('UI.ui', self)
-        self.pushButton.clicked.connect(self.draw)
-        # Обратите внимание: имя элемента такое же как в QTDesigner
+        self.draw = False
+        self.pushButton.clicked.connect(self.check)
 
-    def draw(self):
-        qp = QPainter(self)
-        qp.setPen(QPen(Qt.yellow, 3, Qt.SolidLine))
-        qp.setBrush(QColor(60, 10, 100))
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing, True)
+        painter.setPen(QPen(Qt.yellow, 12, Qt.SolidLine))
+        painter.setBrush(QBrush(Qt.yellow, Qt.SolidPattern))
+        if self.draw:
+            for i in range(random.randint(2, 20)):
+                h = random.randint(10, 50)
+                x = random.randint(10, 400)
+                y = random.randint(10, 300)
+                painter.drawEllipse(x, y, h, h)
+            self.draw = False
 
-        for i in range(random.randint(2, 20)):
-            h = random.randint(10, 50)
-            x = random.randint(10, 400)
-            y = random.randint(10, 300)
-            qp.drawEllipse(x, y, h, h)
+    def check(self):
+        self.draw = True
+        self.update()
 
 
 if __name__ == '__main__':
